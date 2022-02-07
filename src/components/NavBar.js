71cebd42df13493629b85bb08b7149294f806bbb
Mariano San Juan <s.jmariano@gmail.com>
2022-02-07 18:14:21 -0300

    toca estilos de navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import { DropdownButton, Dropdown } from "react-bootstrap";
+import { DropdownButton, Dropdown, Navbar, Container  } from "react-bootstrap";
 //import { GiHamburguerMenu } from "react-icons/gi";
 
 export const NavBar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Navbar className='navbar' variant="">
+        <Container className='justify-content-center'>
+          <Navbar.Brand href="#home">
+          <DropdownButton  className='dropButton' variant="outline" size='lg' title="Servicios" >
+                <Dropdown.Item href="#">Inflables</Dropdown.Item>
+                <Dropdown.Item href="#">Plaza blanda</Dropdown.Item>
+                <Dropdown.Item href="#">Juegos</Dropdown.Item>
+            </DropdownButton>
+          </Navbar.Brand>
+          <Navbar.Brand href="#home" className='prueba'> Combos </Navbar.Brand>
+          <Navbar.Brand href="#home" className='prueba'> Contacto </Navbar.Brand>
+          
+        </Container>
+      </Navbar>
+      
+
+    </>
+    )
+};
+
+/*
+<nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid d-flex justify-content-center style-navbar" >       
             <DropdownButton  className='dropButton' variant="outline" size='lg' title="Servicios" >
                 <Dropdown.Item href="#">Inflables</Dropdown.Item>
@@ -16,7 +37,4 @@ export const NavBar = () => {
             <button variant="outline" size='lg'> Contacto </button>
         </div>
     </nav>
-
-    </>
-    )
-};
+    */
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -36,15 +36,4 @@ export const TopBar = () => {
   )
 };
 
-/*<nav className="navbar navbar-expand-lg navbar-light bg-light ">
-        <div className="container-fluid topbar" >
-          <span className='navbar-brand' >
-            <img src={ whatsapp } width="30" height="24" /> 11-5739-7625 / 11-4448-5829
-          </span>
-          <a href='#'>
-            <img src={ instagram } width="30" height="24"  /> @lospelonesok
-          </a>
-        </div>
-      </nav>
-*/
 
