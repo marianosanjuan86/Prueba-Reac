d1d810bd430378adc6111aa6d9e5055e1ed54912
Mariano San Juan <s.jmariano@gmail.com>
2022-02-07 17:30:03 -0300

    crea el componente topbar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import whatsapp from "../Images/whatsapp.png";
 import instagram from "../Images/instagram.png";
+import { Navbar, Container } from "react-bootstrap";
 
 export const TopBar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
+      <Navbar bg="dark" variant="">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src={ whatsapp }
+              width="30"
+              height="24"
+              className="d-inline-block align-top"
+              />{' '}
+              11-5739-7625 / 11-4448-5829
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+
+    </>
+
+  )
+};
+
+/*<nav className="navbar navbar-expand-lg navbar-light bg-light ">
+        <div className="container-fluid topbar" >
           <span className='navbar-brand' >
             <img src={ whatsapp } width="30" height="24" /> 11-5739-7625 / 11-4448-5829
           </span>
@@ -15,10 +36,5 @@ export const TopBar = () => {
           </a>
         </div>
       </nav>
-
-    </>
-
-  )
-};
-
+*/
 
