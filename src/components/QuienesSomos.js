7892d02a42914f6637af9076dfe8017f19e1a7af
Mariano San Juan <s.jmariano@gmail.com>
2022-02-09 17:14:14 -0300

    agrega texto a QuienesSomos

--- src/components/QuienesSomos.js
+++ src/components/QuienesSomos.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Container, Row, Col } from "react-bootstrap";
 
 export const QuienesSomos = () => {
   return (
-    <>
+    <>  
+      <Container>
+        <Row>
+          <Col>leo duis ut diam quam nulla porttitor massa id neque aliquam vestibulum morbi blandit cursus risus at ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo integer malesuada nunc vel risus commodo viverra maecenas accumsan lacus vel facilisis volutpat est velit egestas dui id ornare arcu odio ut sem nulla pharetra diam sit amet nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit sed vulputate mi sit amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin
+          </Col>
+          <Col>
+              Holasjjdasjdka
+          </Col>
+        </Row>
+        
+
+      </Container>
+
    
 
     </>
