ced1ffad18d02139d57aeb43c358878238fa87cc
Mariano San Juan <s.jmariano@gmail.com>
2022-02-09 12:03:40 -0300

    crea el componente Titulo

---
+++ src/components/Titulo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Titulo = () => {
+
+  return (
+  <>
+        <div className='titulo' >
+            <h1> LOS PELONES </h1>
+            <h3> Llevamos la diversion a tu fiesta!!!</h3>
+        </div>
+
+  </>
+  )
+};
