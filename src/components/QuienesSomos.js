d9205b6c36f48130d1f3161c34e5f86aefe7c4b7
Mariano San Juan <s.jmariano@gmail.com>
2022-02-10 23:23:24 -0300

    modifica QuienesSomos

--- src/components/QuienesSomos.js
+++ src/components/QuienesSomos.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from "react-bootstrap";
+import video1 from "../videos/video1.mp4";
 
 export const QuienesSomos = () => {
   return (
@@ -9,7 +10,7 @@ export const QuienesSomos = () => {
           <Col>leo duis ut diam quam nulla porttitor massa id neque aliquam vestibulum morbi blandit cursus risus at ultrices mi tempus imperdiet nulla malesuada pellentesque elit eget gravida cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies leo integer malesuada nunc vel risus commodo viverra maecenas accumsan lacus vel facilisis volutpat est velit egestas dui id ornare arcu odio ut sem nulla pharetra diam sit amet nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit sed vulputate mi sit amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada proin
           </Col>
           <Col>
-              Holasjjdasjdka
+              <video src={ video1 } autoplay muted loop width="200" height="200" ></video>
           </Col>
         </Row>
         
